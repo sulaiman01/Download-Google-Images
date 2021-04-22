dca8f127af6c71747054bccf34d0fdf0a13d8f46
sulaiman01 <mohammadsulaiman01@gmail.com>
2021-04-22 21:59:00 +0530

    added new method

--- console.js
+++ console.js
@@ -13,3 +13,11 @@ hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
 hiddenElement.target = '_blank';
 hiddenElement.download = 'urls.txt';
 hiddenElement.click();
+
+// Another Method of getting the urls 
+var urls=Array.from(document.querySelectorAll('.rg_i')).map(el=> el.hasAttribute('data-src')?el.getAttribute('data-src'):el.getAttribute('data-iurl'));
+var hiddenElement = document.createElement('a');
+hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(urls.join('\n'));
+hiddenElement.target = '_blank';
+hiddenElement.download = 'myFile.txt';
+hiddenElement.click();
